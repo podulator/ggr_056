14b897b1f028f529537d5c3b49f0c212f7478260
podulator <github-account@podulator.com>
2019-10-15 18:39:45 +0200

    buildroot friendly build script

--- gcw0.sh
+++ gcw0.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-mipsel-linux-g++ game.cpp -o game.dge \
+#mipsel-linux-g++ game.cpp -o gng \
+#	-std=gnu++98 \
+#	-I/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include/allegro \
+#	-g -O2 -mips32 \
+#	-Wno-write-strings \
+#	`/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/bin/allegro-config --libs`
+
+mipsel-linux-g++ game.cpp -o gng \
 	-std=gnu++98 \
 	-g -O2 -mips32 \
 	-Wno-write-strings \
 	`allegro-config --libs`
-
-#	-g -O2 -mips32 \
-#	-Wno-write-strings \
-#	-I../allegro/lib/Headers \
-#	-lm -lpthread -lrt -lts -ldl \
-#	-lalleg-debug \
-#	-lasound \
-#	-L../allegro/lib/
